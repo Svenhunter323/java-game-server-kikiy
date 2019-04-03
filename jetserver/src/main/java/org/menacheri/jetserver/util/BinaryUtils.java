d42725d8c92f74f495e5e5335584a3130153ee36
Khamtanh <laotrader23@gmail.com>
2019-04-03 12:53:48 +0700

    Add retry logic to bot

---
+++ jetserver/src/main/java/org/menacheri/jetserver/util/BinaryUtils.java
@@ -0,0 +1,43 @@
+package org.menacheri.jetserver.util;
+
+public class BinaryUtils
+{
+
+	private static final String HEXES = "0123456789ABCDEF";
+
+	public static String getHexString(byte[] raw)
+	{
+		return getHexString(raw, null);
+	}
+
+	public static String getHexString(byte[] raw, String separator)
+	{
+		boolean sep = (null != separator) && !("".equals(separator));
+
+		if (raw == null)
+		{
+			return null;
+		}
+		final StringBuilder hex = new StringBuilder(2 * raw.length);
+		for (final byte b : raw)
+		{
+			hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(
+					HEXES.charAt((b & 0x0F)));
+			if (sep)
+			{
+				hex.append(separator);
+			}
+		}
+		return hex.toString();
+	}
+}
+# Change 0 on 2019-05-30
+# Change 1 on 2019-07-05
+# Change 0 on 2020-04-07
+# Change 0 on 2020-10-17
+# Change 0 on 2020-10-29
+# Change 0 on 2020-11-12
+# Change 0 on 2020-12-29
+# Change 1 on 2021-03-27
+# Change 2 on 2021-08-06
+# Change 0 on 2019-04-03
