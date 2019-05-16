1e6d84c8019650c93b98011c2d0bd36de133a207
Khamtanh <laotrader23@gmail.com>
2019-05-16 16:03:32 +0700

    Enhance docstring documentation

---
+++ jetclient/src/main/java/org/menacheri/jetclient/event/NetworkEvent.java
@@ -0,0 +1,29 @@
+package org.menacheri.jetclient.event;
+
+import org.menacheri.jetclient.communication.DeliveryGuaranty;
+
+/**
+ * This interface is specifically used for events that will get transmitted to
+ * remote machine/vm. It contains the {@link DeliveryGuaranty} associated with
+ * the event so that messages can be transmitted either using TCP or UDP
+ * transports based on the guaranty defined. Implementations can use RELIABLE as
+ * default.
+ * 
+ * @author Abraham Menacherry
+ * 
+ */
+public interface NetworkEvent extends Event
+{
+	DeliveryGuaranty getDeliveryGuaranty();
+
+	void setDeliveryGuaranty(DeliveryGuaranty deliveryGuaranty);
+}
+# Change 0 on 2019-03-26
+# Change 3 on 2019-04-25
+# Change 0 on 2020-11-19
+# Change 0 on 2020-12-26
+# Change 0 on 2021-01-15
+# Change 0 on 2021-03-27
+# Change 0 on 2021-04-15
+# Change 0 on 2021-07-08
+# Change 1 on 2019-05-16
