e7840df6737a8e1fc2d92170e8e7c7fa955d8036
Khamtanh <laotrader23@gmail.com>
2019-08-08 17:14:43 +0700

    Fix API key handling

---
+++ jetclient/src/main/java/org/menacheri/jetclient/app/PlayerSession.java
@@ -0,0 +1,21 @@
+package org.menacheri.jetclient.app;
+
+/**
+ * This interface abstracts a user session to a {@link Game}.
+ * 
+ * @author Abraham Menacherry
+ * 
+ */
+public interface PlayerSession extends Session
+{
+	/**
+	 * Each session is associated with a {@link Player}. This is the actual
+	 * human or machine using this session.
+	 * 
+	 * @return Returns that associated Player object or null if it is not
+	 *         associated yet.
+	 */
+	public abstract Player getPlayer();
+
+}
+# Change 2 on 2019-08-08
